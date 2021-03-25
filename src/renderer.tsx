8fadfe579b3ac118f22fd687d32d0afe95a4e376
Micchili <wot.dannbo@gmail.com>
2021-03-26 01:26:55 +0900

    first commit

---
+++ src/renderer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Disk from "./components/Disk"
+
+const App: React.FC = () => {
+    return(
+        <>
+            <p>ホゲヌルプレイヤー</p>
+        </>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
