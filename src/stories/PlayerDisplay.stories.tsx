cd2305701f8aedae18ff180cfba09ae9a56a6b74
Micchili <wot.dannbo@gmail.com>
2021-07-18 01:58:10 +0900

    [add] プレイヤー部分のディスプレイのストーリーを作成

--- src/stories/PlayerDisplay.stories.tsx
+++ src/stories/PlayerDisplay.stories.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
 
-import PlayerDisplay , {IProps} from '../atoms/PlayerDisplay'
+import {PlayerDisplay, IProps} from '../../src/atoms/PlayerDisplay'
 
 export default {
-  title: 'Atoms/PlayerDisplay',
+  title: 'Componets/PlayerDisplay',
   component: PlayerDisplay,
 } as Meta;
 
-const Template: Story<IProps> = (args) => <PlayerDisplay text={args.text} />;
+const Template: Story<IProps> = (args) => <PlayerDisplay videoUrl={args.videoUrl} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-    text: '1. 君の知らない物語　-Youtube-  Albam1',
-  };
+    videoUrl: 'https://www.youtube.com/watch?v=ony539T074w',
+};
